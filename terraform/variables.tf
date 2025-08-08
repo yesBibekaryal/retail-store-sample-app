3c1399f8a991a39ae218e94fda9c1156e1ae77fc
Bibek Aryal <125453535+yesBibekaryal@users.noreply.github.com>
2025-08-08 15:46:26 +1000

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@
 variable "aws_region" {
   description = "AWS region where resources will be created"
   type        = string
-  default     = "us-west-2"
+  default     = "eu-west-1"
 }
 
 variable "cluster_name" {
